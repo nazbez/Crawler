19877772c0007adc872cfb747166577ea82ded1d
Nazar Bezuhlyi <79589474+nazbez@users.noreply.github.com>
2021-08-23 12:31:35 +0300

    fix conflict in home controller

--- Crawler.WebApplication/Controllers/HomeController.cs
+++ Crawler.WebApplication/Controllers/HomeController.cs
@@ -19,9 +19,9 @@ namespace Crawler.WebApplication.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            var tests = _dbHandler.GetAllTests().Select(x => new TestModel() { Id = x.Id, Url = x.Url, SaveTime = (DateTime)x.SaveTime });
+            var testsView = _dbService.GetTests();
 
-            return View(new TestsViewModel() { Tests = tests});
+            return View(testsView);
         }
 
         [HttpPost]
